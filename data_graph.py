eb679601a4e9617812c5d675eacd6a9cb4629ae7
SansNeal <62467577+SansNeal@users.noreply.github.com>
2021-05-03 12:01:08 +0530

    Update data_graph.py

--- data_graph.py
+++ data_graph.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 
 df =  pd.read_csv("data.csv")
-fig =px.line(df, x = "Date", y ="Cases", color = "Country", title = "DATA VISUALISATION")
+fig =px.scatter(df, x = "Date", y ="Cases", color = "Country", title = "DATA VISUALISATION")
 fig.show()
